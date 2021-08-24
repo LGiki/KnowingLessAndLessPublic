6522330b435c302ae339e20a36a9df20a82af31c
LGiki <qq547221432@gmail.com>
2021-08-24 23:53:18 +0800

    Update `one`

--- one/_next/static/6G-xRi474U6y2t33bFtcd/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/424-6e952c4ea41eb4f467a9.js","static/chunks/pages/index-ff3558c9d433be3599e5.js"],"/_error":["static/chunks/pages/_error-eb7f79f2af83e8ddf5e2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
